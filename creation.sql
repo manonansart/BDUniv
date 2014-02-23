aeb0d7b91008305ab37cd0c31b4af534c5c888e2
Antoine Augusti <antoine@augusti.fr>
2014-02-23 17:56:57 +0100

    Commit initial

---
+++ creation.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS reserve;
+DROP TABLE IF EXISTS passePar;
+DROP TABLE IF EXISTS appartient;
+DROP TABLE IF EXISTS tache;
+DROP TABLE IF EXISTS personne;
+DROP TABLE IF EXISTS piece;
+
+CREATE TABLE piece(
+	idP VARCHAR(10) PRIMARY KEY NOT NULL,
+	type VARCHAR(15),
+	gps_lat REAL,
+	gps_long REAL,
+	CHECK (type IN ('Bureau', 'Salle de Cours', 'Autre'))
+);
+
+CREATE TABLE personne(
+	idPers VARCHAR(10) PRIMARY KEY NOT NULL,
+	nom VARCHAR(20),
+	grade VARCHAR(10),
+	CHECK (grade IN ('Etudiant', 'MCF', 'PU', 'BIATOSS', 'PE'))
+);
+
+CREATE TABLE tache(
+	date date,
+	tache VARCHAR(15),
+	idPers VARCHAR(10) references personne(idPers) ON DELETE CASCADE,
+	PRIMARY KEY (dateTache, description, idPers),
+	CHECK (description IN ('Enseignement', 'Recherche', 'Réunion'))
+);
+
+CREATE TABLE appartient(
+	idP VARCHAR(10) references piece(idP) ON DELETE CASCADE,
+	idPers VARCHAR(10) references personne(idPers) ON DELETE CASCADE,
+	PRIMARY KEY(idP, idPers)
+);
+
+CREATE TABLE passePar(
+	idP VARCHAR(10) references piece(idP) ON DELETE CASCADE,
+	idPers VARCHAR(10) references personne(idPers) ON DELETE CASCADE,
+	date date,
+	PRIMARY KEY(idP, idPers)
+);
+
+CREATE TABLE reserve(
+	idP VARCHAR(10) references piece(idP) ON DELETE CASCADE,
+	idPers VARCHAR(10) references personne(idPers) ON DELETE CASCADE,
+	date date,
+	PRIMARY KEY(idP, idPers)
+);
+
+-- Les index
+CREATE INDEX 
