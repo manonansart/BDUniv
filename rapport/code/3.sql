7c99220db080820de20293f60d07d97cc699a751
Antoine Augusti <antoine@augusti.fr>
2014-03-15 09:49:07 +0100

    Renommage d'un fichier et corrections mineures dans le rapport

--- rapport/code/3.sql
+++ rapport/code/3.sql
@@ -1,28 +1,24 @@
 CREATE OR REPLACE FUNCTION estIntru(dateIntru date, pieceIntru VARCHAR(10), idIntru VARCHAR(10))
 RETURNS Integer AS $$
-
 DECLARE
 	gradeIntru VARCHAR;
 	proprio VARCHAR;
 	typePiece VARCHAR;
 	passageProprio Integer;
-
-BEGIN
 	
+BEGIN
 	SELECT p.grade INTO gradeIntru
 	FROM personne p
 	WHERE p.idPers = idIntru;
-	
+
 	SELECT pi.type INTO typePiece
 	FROM piece pi
 	WHERE pi.idP = pieceIntru;
 
 
-	
 	IF (gradeIntru = 'PE' OR typePiece != 'Bureau') THEN
 		RETURN 0;
 	ELSE
-	
 		SELECT count(p.idP) INTO passageProprio
 		FROM passePar p
 		WHERE p.date = dateIntru
@@ -31,11 +27,11 @@ BEGIN
 				(SELECT a.idPers
 				FROM appartient a
 				WHERE a.idP = pieceIntru);
-	
 		IF (passageProprio > 0) THEN
 			RETURN 0;
 		END IF;
 	END IF;
-	
+
 	RETURN 1;
-END $$ LANGUAGE 'plpgsql';
+
+END $$ LANGUAGE 'plpgsql'
