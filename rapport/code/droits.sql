dedae634b65532eb8fbfd4862f106102a687982c
Antoine Augusti <antoine@augusti.fr>
2014-03-21 13:02:15 +0100

    Paragraphe sur les droits, ajout de précisions dans la création du modèle relationnel

---
+++ rapport/code/droits.sql
@@ -0,0 +1,17 @@
+-- Les droits
+GRANT SELECT, UPDATE ON tache TO grtt42;
+GRANT SELECT, UPDATE ON passepar TO grtt42;
+GRANT SELECT, UPDATE ON piece TO grtt42;
+GRANT SELECT, UPDATE ON personne TO grtt42;
+GRANT SELECT, UPDATE ON rapport_activite TO grtt42;
+GRANT SELECT, UPDATE ON intrusion TO grtt42;
+GRANT SELECT, UPDATE ON appartient TO grtt42;
+GRANT SELECT, UPDATE ON reservation TO grtt42;
+GRANT SELECT ON tache TO grtt11;
+GRANT SELECT ON passepar TO grtt11;
+GRANT SELECT ON piece TO grtt11;
+GRANT SELECT ON personne TO grtt11;
+GRANT SELECT ON rapport_activite TO grtt11;
+GRANT SELECT ON intrusion TO grtt11;
+GRANT SELECT ON appartient TO grtt11;
+GRANT SELECT ON reservation TO grtt11;
